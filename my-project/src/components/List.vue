688c806276c706de756efe6aaffacf158fe8177e
heine.shimizu <heine.shimizu@gmail.com>
2019-08-27 20:15:06 +0900

    削除機能を追加

--- my-project/src/components/List.vue
+++ my-project/src/components/List.vue
@@ -1,7 +1,15 @@
 <template lang="pug">
   div
-    ul(v-for="item in items")
-      ListContent(:item="item")
+    input(v-model="newContent")
+    div new ? {{ newContent }}
+    div(@click="addContent") add
+    ul(v-for="(item, index) in items")
+      div.v-ul-list
+        div.v-ul-check(@click="checkContent(index)")
+        ListContent(
+          :item="item"
+          )
+        div(@click="deleteContent(index)") delete
 </template>
 
 <script>
@@ -11,28 +19,36 @@ export default {
   data () {
     return {
       items: ['test', 'test2'],
+      newContent: '',
     }
   },
   components: {
     ListContent,
   },
+  methods: {
+    addContent: function() {
+      if(this.newContent !== '') {
+        this.items.push(this.newContent);
+      }
+    },
+    deleteContent: function(index) {
+      this.items.splice(index, 1);
+    },
+    checkContent: function(index) {
+
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="scss">
+  .v-ul {
+    &-list {
+      display: flex;
+    }
+    &-check {
+      
+    }
+  }
 </style>
